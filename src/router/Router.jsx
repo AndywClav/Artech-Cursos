fba739d0b2c900eb839243272524e3946bd818dc
Andrés F. Clavijo R. <andresclavijo0822@gmail.com>
2025-03-21 19:28:30 -0300

    [Test] deploy

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,7 +4,7 @@ import { Cursos, Curso } from "../pages";
 
 function Router() {
   return (
-    <BrowserRouter basename={"/pescar/artech/"}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Navigate to="/cursos" />} />
 
